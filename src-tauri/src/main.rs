d50a15403ae09306b8a4331959191edb6115228e
Glitch752xx <xxGlitch752xx@gmail.com>
2022-08-15 21:14:43 -0500

    Added rust types

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@
 use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent};
 use tauri::Manager;
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 fn main() {
   println!("Main ran");
@@ -15,7 +15,7 @@ fn main() {
     .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
       println!("{}, {argv:?}, {cwd}", app.package_info().name);
       let window = app.get_window("main").unwrap();
-      window.show();
+      window.show().unwrap();
     }))
     .invoke_handler(tauri::generate_handler![update_macros])
     .system_tray(
@@ -47,15 +47,59 @@ fn main() {
       }
       _ => {}
     })
-    .setup(|_| {
-      initialize_macro_listeners();
-      Ok(())
-    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+use serde::{Deserialize, Serialize};
+use serde_json::value::Value;
+
+type Macros = Vec<Macro>;
+
+#[derive(Serialize, Deserialize)]
+struct Macro {
+  name: String,
+  description: String,
+  macro_: MacroMacro,
+}
+
+#[derive(Serialize, Deserialize)]
+struct MacroMacro {
+  initiators: Vec<Initiator>,
+  functions: Vec<Function>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Initiator {
+  type_: String,
+  data: Value,
+  executes: Vec<Execution>
+}
+
+#[derive(Serialize, Deserialize)]
+struct Execution {
+  type_: String,
+  data: Value,
+  code_inside: Value
+}
+
+#[derive(Serialize, Deserialize)]
+struct Function {
+  name: String,
+  parameters: Vec<Parameter>,
+  executes: Vec<Execution>
+}
+
+#[derive(Serialize, Deserialize)]
+struct Parameter {
+  name: String,
+  type_: String,
+  default_value: String,
+}
 
 #[tauri::command]
-fn update_macros(macros: String) {
-  println!("{}", macros);
+fn update_macros(macros: Macros) {
+  println!("Macros updated");
+  for macro_ in macros {
+    println!("{}", macro_.name);
+  }
 }
