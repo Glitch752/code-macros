319ff7fb61527b30fd93a0e05c6caec99491cc21
Glitch752 <xxGlitch752xx@gmail.com>
2022-10-14 12:12:31 -0500

    Added styling to the settings

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -20,16 +20,25 @@
 <template>
   <h1>Settings</h1>
 
-  <span>Theme</span>
-  <select :value="theme" @change="selectTheme">
-    <option value="darkTheme">Dark theme</option>
-    <option value="lightTheme">Light theme</option>
-  </select>
+  <div class="setting">
+    <span>Theme</span>
+    <span class="dropdown">
+      <span></span>
+      <select :value="theme" @change="selectTheme">
+        <option value="darkTheme">Dark theme</option>
+        <option value="lightTheme">Light theme</option>
+      </select>
+    </span>
+  </div>
 
   <span class="backButton" @click="goBack">&lt;</span>
 </template>
 
 <style scoped>
+  .setting {
+    text-align: center;
+    margin-top: 5px;
+  }
   .backButton {
     position: absolute;
     top: -10px;
@@ -41,5 +50,29 @@
   }
   .backButton:hover {
     color: var(--primary-text);
+  }
+  .dropdown {
+    position: relative;
+    margin-left: 10px;
+  }
+  .dropdown select {
+    appearance: none;
+    background-color: var(--dark-background);
+    outline: none;
+    border: none;
+    border-radius: 0;
+    padding: 5px 25px 5px 10px;
+    color: var(--primary-text);
+    font-size: 14px;
+  }
+  .dropdown span {
+    width: 0; 
+    height: 0; 
+    border-left: 6px solid transparent;
+    border-right: 6px solid transparent;
+    border-top: 6px solid var(--primary-text);
+    position: absolute;
+    top: 10px;
+    right: 10px;
   }
 </style>
