1a574c0baff6742da7b21133479f288b6a706673
Glitch752 <xxGlitch752xx@gmail.com>
2023-01-13 12:26:46 -0600

    Split expression parsing into separate file

--- src-tauri/src/execute.rs
+++ src-tauri/src/execute.rs
@@ -1,4 +1,5 @@
 pub mod string_parser;
+pub mod expression;
 
 use serde::{ Deserialize, Serialize };
 
@@ -18,7 +19,9 @@ use std::thread;
 use std::cmp::Ordering;
 use std::{collections::HashMap, path::Path};
 
+use self::expression::Expression;
 use self::string_parser::parse_string;
+use self::expression::evaluate_expression;
 
 // TODO: REACTOR: Split this into multiple files for parsing conditions, executions, etc.
 
@@ -382,25 +385,6 @@ pub enum Condition {
     Number { value: f64 },
     Variable { variable: String },
     Expression { expression: Expression }
-}
-
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-#[serde(tag = "type_")]
-#[serde(rename_all = "lowercase")]
-pub enum Expression {
-    Number { value: f64 },
-    Variable { variable: String },
-    Arithmetic {
-        left: Box<Expression>, 
-        kind: String, 
-        right: Box<Expression> 
-    },
-    Bitwise {
-        left: Box<Expression>, 
-        kind: String, 
-        right: Box<Expression> 
-    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
@@ -951,69 +935,6 @@ fn evaluate_condition(condition: &Condition, variables: &mut Variables) -> Condi
             return Condition::Number{ value: get_expression_number(
                 evaluate_expression(expression, variables)
             ) }
-        }
-    }
-}
-
-fn evaluate_expression(expression: &Expression, variables: &mut Variables) -> Expression {
-    match expression {
-        Expression::Number { value: _ } => {
-            return expression.clone();
-        },
-        Expression::Variable { variable } => {
-            return Expression::Number{ value: get_variable_number(
-                get_variable(variables, variable.to_string()).unwrap_or( &Variable::new(VariableValue::Number(0.0)) ).value.clone()
-            ) };
-        },
-        Expression::Arithmetic { left, kind, right } => {
-            let left_result: f64 = get_expression_number(evaluate_expression(left, variables));
-            let right_result: f64 = get_expression_number(evaluate_expression(right, variables));
-            match kind.as_str() {
-                "addition" => {
-                    return Expression::Number{ value: left_result + right_result };
-                },
-                "subtraction" => {
-                    return Expression::Number{ value: left_result - right_result };
-                },
-                "division" => {
-                    return Expression::Number{ value: left_result / right_result };
-                },
-                "multiplication" => {
-                    return Expression::Number{ value: left_result * right_result };
-                },
-                "modulo" => {
-                    return Expression::Number{ value: left_result % right_result };
-                },
-                "exponent" => {
-                    return Expression::Number{ value: left_result.powf(right_result) };
-                },
-                _ => todo!()
-            }
-        },
-        Expression::Bitwise { left, kind, right } => {
-            let left_result: i64 = get_expression_number(evaluate_expression(left, variables)).round() as i64;
-            let right_result: i64 = if kind == "not" { 0 } else { get_expression_number(evaluate_expression(right, variables)).round() as i64 };
-            match kind.as_str() {
-                "and" => {
-                    return Expression::Number{ value: (left_result & right_result) as f64 };
-                },
-                "or" => {
-                    return Expression::Number{ value: (left_result | right_result) as f64 };
-                },
-                "xor" => {
-                    return Expression::Number{ value: (left_result ^ right_result) as f64 };
-                },
-                "not" => {
-                    return Expression::Number{ value: (!right_result) as f64 };
-                },
-                "leftshift" => {
-                    return Expression::Number{ value: (left_result << right_result) as f64 };
-                },
-                "signrightshift" => {
-                    return Expression::Number{ value: (left_result >> right_result) as f64 };
-                },
-                _ => todo!()
-            }
         }
     }
 }
---
+++ src-tauri/src/execute/expression.rs
@@ -0,0 +1,84 @@
+use serde::{Serialize, Deserialize};
+
+use super::{Variables, get_variable_number, get_variable, get_expression_number, Variable, VariableValue};
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(tag = "type_")]
+#[serde(rename_all = "lowercase")]
+pub enum Expression {
+    Number { value: f64 },
+    Variable { variable: String },
+    Arithmetic {
+        left: Box<Expression>, 
+        kind: String, 
+        right: Box<Expression> 
+    },
+    Bitwise {
+        left: Box<Expression>, 
+        kind: String, 
+        right: Box<Expression> 
+    }
+}
+
+pub fn evaluate_expression(expression: &Expression, variables: &mut Variables) -> Expression {
+    match expression {
+        Expression::Number { value: _ } => {
+            return expression.clone();
+        },
+        Expression::Variable { variable } => {
+            return Expression::Number{ value: get_variable_number(
+                get_variable(variables, variable.to_string()).unwrap_or( &Variable::new(VariableValue::Number(0.0)) ).value.clone()
+            ) };
+        },
+        Expression::Arithmetic { left, kind, right } => {
+            let left_result: f64 = get_expression_number(evaluate_expression(left, variables));
+            let right_result: f64 = get_expression_number(evaluate_expression(right, variables));
+            match kind.as_str() {
+                "addition" => {
+                    return Expression::Number{ value: left_result + right_result };
+                },
+                "subtraction" => {
+                    return Expression::Number{ value: left_result - right_result };
+                },
+                "division" => {
+                    return Expression::Number{ value: left_result / right_result };
+                },
+                "multiplication" => {
+                    return Expression::Number{ value: left_result * right_result };
+                },
+                "modulo" => {
+                    return Expression::Number{ value: left_result % right_result };
+                },
+                "exponent" => {
+                    return Expression::Number{ value: left_result.powf(right_result) };
+                },
+                _ => todo!()
+            }
+        },
+        Expression::Bitwise { left, kind, right } => {
+            let left_result: i64 = get_expression_number(evaluate_expression(left, variables)).round() as i64;
+            let right_result: i64 = if kind == "not" { 0 } else { get_expression_number(evaluate_expression(right, variables)).round() as i64 };
+            match kind.as_str() {
+                "and" => {
+                    return Expression::Number{ value: (left_result & right_result) as f64 };
+                },
+                "or" => {
+                    return Expression::Number{ value: (left_result | right_result) as f64 };
+                },
+                "xor" => {
+                    return Expression::Number{ value: (left_result ^ right_result) as f64 };
+                },
+                "not" => {
+                    return Expression::Number{ value: (!right_result) as f64 };
+                },
+                "leftshift" => {
+                    return Expression::Number{ value: (left_result << right_result) as f64 };
+                },
+                "signrightshift" => {
+                    return Expression::Number{ value: (left_result >> right_result) as f64 };
+                },
+                _ => todo!()
+            }
+        }
+    }
+}
